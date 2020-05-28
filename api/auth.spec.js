bdeb64f28765f5550e855db64c52452d7e74616c
alanblee <alanbenlee12@gmail.com>
2020-05-27 20:45:42 -0700

    added tests for PUT route to update a users information

--- api/auth.spec.js
+++ api/auth.spec.js
@@ -2,88 +2,202 @@ const request = require("supertest");
 const server = require("./server");
 const db = require("../data/dbConfig");
 
-
 afterEach(async () => {
   await db("users").del();
 });
 
 describe("AUTH ROUTES", () => {
   //Test POST /register
+  describe("POST route /api/user", () => {
+    //responds with an a status 400 if username / password is not provided
+    it("should respond with a 400 status code if password is not provided", async () => {
+      const expectedStatus = 400;
+      const userInfo = {
+        username: "mellow",
+        // password: "password",
+        //leaving password out to pass the test
+      };
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(userInfo);
+      expect(response.status).toEqual(expectedStatus);
+    });
+    it("should respond with a 400 status code if username is not provided", async () => {
+      const expectedStatus = 400;
+      const userInfo = {
+        // username: "mellow",
+        password: "password",
+        //leaving username out to pass the test
+      };
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(userInfo);
+      expect(response.status).toEqual(expectedStatus);
+    });
+    it("should respond with a 400 status code if phone_number is not provided", async () => {
+      const expectedStatus = 400;
+      const userInfo = {
+        // username: "mellow",
+        // password: "password",
+        phone_number: 111 - 111 - 1111,
+        //leaving username and password out to pass the test
+      };
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(userInfo);
+      expect(response.status).toEqual(expectedStatus);
+    });
 
-  //responds with an a status 400 if username / password is not provided
-  it("should respond with a 400 status code if password is not provided", async () => {
-    const expectedStatus = 400;
-    const userInfo = {
-      username: "mellow",
-      // password: "password",
-      //leaving password out to pass the test
-    };
-    const response = await request(server)
-      .post("/api/auth/register")
-      .send(userInfo);
-    expect(response.status).toEqual(expectedStatus);
-  });
-  it("should respond with a 400 status code if username is not provided", async () => {
-    const expectedStatus = 400;
-    const userInfo = {
-      // username: "mellow",
-      password: "password",
-      //leaving username out to pass the test
-    };
-    const response = await request(server)
-      .post("/api/auth/register")
-      .send(userInfo);
-    expect(response.status).toEqual(expectedStatus);
-  });
-  it("should respond with a 400 status code if phone_number is not provided", async () => {
-    const expectedStatus = 400;
-    const userInfo = {
-      // username: "mellow",
-      // password: "password",
-      phone_number: 111 - 111 - 1111,
-      //leaving username and password out to pass the test
-    };
-    const response = await request(server)
-      .post("/api/auth/register")
-      .send(userInfo);
-    expect(response.status).toEqual(expectedStatus);
+    //registers a new user
+    it("should respond with status 201 on success", async () => {
+      const expectedStatus = 201;
+      const userInfo = {
+        username: "method",
+        password: "password",
+        phone_number: "111-111-1111",
+      };
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(userInfo);
+      expect(response.status).toEqual(expectedStatus);
+    });
+    it("should respond with an object in body", async () => {
+      const expectedStatus = 201;
+      const userInfo = {
+        username: "method",
+        password: "password",
+        phone_number: "111-111-1111",
+      };
+      const response = await request(server)
+        .post("/api/auth/register")
+        .send(userInfo);
+      //check for id
+      expect(response.body.id).toBeDefined();
+      //check for username
+      expect(response.body.username).toBeDefined();
+      //check for hashed password
+      expect(response.body.password).toBeDefined();
+      //check for hashed password
+      expect(response.body.phone_number).toBeDefined();
+    });
   });
 
-  //registers a new user
-  it("should respond with status 201 on success", async () => {
-    const expectedStatus = 201;
-    const userInfo = {
-      username: "method",
-      password: "password",
-      phone_number: "111-111-1111",
-    };
-    const response = await request(server)
-      .post("/api/auth/register")
-      .send(userInfo);
-    expect(response.status).toEqual(expectedStatus);
-  });
-  it("should respond with an object in body", async () => {
-    const expectedStatus = 201;
-    const userInfo = {
-      username: "method",
-      password: "password",
-      phone_number: "111-111-1111",
-    };
-    const response = await request(server)
-      .post("/api/auth/register")
-      .send(userInfo);
-    //check for id
-    expect(response.body.id).toBeDefined();
-    //check for username
-    expect(response.body.username).toBeDefined();
-    //check for hashed password
-    expect(response.body.password).toBeDefined();
-    //check for hashed password
-    expect(response.body.phone_number).toBeDefined();
+  //Test PUT for updating user
+  describe("PUT route /api/auth/user/:id", () => {
+    let authToken;
+    beforeEach(async () => {
+      await db.migrate
+        .rollback()
+        .then(() => db.migrate.latest())
+        .then(() => db.seed.run());
+
+      await request(server)
+        .post("/api/auth/login")
+        .send({
+          username: "test1",
+          password: "password",
+        })
+        .then((res) => {
+          authToken = res.body.token;
+        });
+    });
+
+    it("should respond with a 400 if username is missing", async () => {
+      const expectedStatus = 400;
+      const userInfo = {
+        // username: "mellow",
+        password: "password",
+        phone_number: "111-111-1111",
+      };
+      const response = await request(server)
+        .put("/api/user/1")
+        .send(userInfo)
+        .set("Authorization", authToken);
+      expect(response.status).toEqual(expectedStatus);
+    });
+    it("should respond with a 400 if password is missing", async () => {
+      const expectedStatus = 400;
+      const userInfo = {
+        username: "mellow",
+        // password: "password",
+        phone_number: "111-111-1111",
+      };
+      const response = await request(server)
+        .put("/api/user/1")
+        .send(userInfo)
+        .set("Authorization", authToken);
+      expect(response.status).toEqual(expectedStatus);
+    });
+    it("should respond with a 400 if phone_number is missing", async () => {
+      const expectedStatus = 400;
+      const userInfo = {
+        username: "mellow",
+        password: "password",
+        // phone_number: "111-111-1111",
+      };
+      const response = await request(server)
+        .put("/api/user/1")
+        .send(userInfo)
+        .set("Authorization", authToken);
+
+      expect(response.status).toEqual(expectedStatus);
+    });
+
+    it("should respond with a 401 status if user is not logged in", async () => {
+      const expectedStatus = 401;
+      const userInfo = {
+        username: "mellow",
+        password: "password",
+        phone_number: "111-111-1111",
+      };
+      const unauthorizedToken = "312313213";
+      const response = await request(server)
+        .put("/api/user/1")
+        .send(userInfo)
+        .set("Authorization", unauthorizedToken);
+
+      expect(response.status).toEqual(expectedStatus);
+    });
+
+    it("should respond with a 200 status if success", async () => {
+      const expectedStatus = 200;
+      const userInfo = {
+        username: "mellow",
+        password: "password",
+        phone_number: "111-111-1111",
+      };
+      const response = await request(server)
+        .put("/api/user/1")
+        .send(userInfo)
+        .set("Authorization", authToken);
+
+      expect(response.status).toEqual(expectedStatus);
+    });
+    it("should respond with an object on success", async () => {
+      const expectedStatus = 200;
+      const userInfo = {
+        username: "mellow",
+        password: "password",
+        phone_number: "111-111-1111",
+      };
+      const response = await request(server)
+        .put("/api/user/1")
+        .send(userInfo)
+        .set("Authorization", authToken);
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          username: response.body.username,
+          password: response.body.password,
+          phone_number: response.body.phone_number,
+          id: response.body.id,
+        })
+      );
+    });
   });
 
   //Test POST for /login
-  describe("/api/auth/login", () => {
+  describe("POST route /api/auth/login", () => {
     //create user and logs in
     it("should create a user and login with that credential and respond with 200", async () => {
       const expectedStatus = 200;
